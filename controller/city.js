3d33984ce185d922da2794e7c5d2f5e058cb9787
aliao <aliao_12345@126.com>
2022-08-05 18:01:49 +0800

    城市控制器编写

--- controller/city.js
+++ controller/city.js
@@ -8,31 +8,40 @@ class City extends LocationComponent {
   constructor () {
     super()
     this.getCity = this.getCity.bind(this)
-    this.getExactLocation = this.getExactAddress.bind(this)
+    this.getExactLocation = this.getExactLocation.bind(this)
     this.getDetailAddress = this.getDetailAddress.bind(this)
   }
+
   async getCityName (req) {
     try {
       // 调用getLocation用ip获取城市名及经纬度信息
       const cityInfo = await this.getLocation(req)
       // 然后将城市名转换为拼音
-      
-
+      let cityNamePinyin = ''
+      pinyin(cityInfo.cityName, {
+        style: 'normal'
+      }).forEach(item => {
+        cityNamePinyin += item[0]
+      })
+      return cityNamePinyin
     } catch (error) {
-      
+      return 'xianshi'
     }
   }
+
   async getCity (req, res, next) {
     const type = req.query.type
     let cityInfo
     try {
       switch (type) {
-        case 'guess':
-          // 获取城市名
-          // 根据城市名去数据库查询该城市相关信息，并将其保存
+        case 'guess': {
+          // 获取城市名拼音
+          // 根据城市名拼音去数据库查询该城市相关信息，并将其保存
           const city = await this.getCityName(req)
+          // 根据城市名拼音去数据库查询城市相关信息
           cityInfo = await CityModel.cityGuess(city)
           break
+        }
         case 'hot':
           cityInfo = CityModel.cityHot()
           break
@@ -54,4 +63,10 @@ class City extends LocationComponent {
       })
     }
   }
+
+  async getExactLocation () {}
+
+  async getDetailAddress () {}
 }
+
+export default new City()
